aeb09440dfdf8fe7f18252fc0f5d8ba45be3afd7
Edder Hernandez Gomez <ehernandezgomez@vassar.edu>
2024-07-03 21:09:23 +0000

    modified main.py to include hooks functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #registration form with no navbar
 
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
@@ -25,8 +26,16 @@ def home():
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
-
-
+    
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/edderregistration/registration')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
